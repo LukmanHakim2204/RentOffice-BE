e23ee0702523874cee45e5bcc1618a0c9cc124e5
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-06-27 20:59:23 +0700

    make booking transaction resouce

---
+++ app/Filament/Resources/BookingTransactionResource.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BookingTransactionResource\Pages;
+use App\Filament\Resources\BookingTransactionResource\RelationManagers;
+use App\Models\BookingTransaction;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BookingTransactionResource extends Resource
+{
+    protected static ?string $model = BookingTransaction::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-calendar-days';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('booking_trx_id')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('phone_number')
+                    ->tel()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('total_amount')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('duration')
+                    ->required()
+                    ->numeric()
+                    ->prefix('Days'),
+                Forms\Components\DatePicker::make('started_at')
+                    ->required(),
+                Forms\Components\DatePicker::make('ended_at')
+                    ->required(),
+                Forms\Components\Select::make('is_paid')
+                    ->options([
+                        true => 'Paid',
+                        false => 'Not Paid',
+                    ]),
+                Forms\Components\Select::make('office_space_id')
+                    ->relationship('officeSpace', 'name')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone_number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('booking_trx_id')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('officeSpace.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total_amount')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('duration')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('started_at')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('ended_at')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('is_paid')
+                    ->boolean()
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->trueIcon('heroicon-s-check-circle')
+                    ->falseIcon('heroicon-s-x-circle')
+                    ->label('Sudah Bayar?'),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBookingTransactions::route('/'),
+            'create' => Pages\CreateBookingTransaction::route('/create'),
+            'edit' => Pages\EditBookingTransaction::route('/{record}/edit'),
+        ];
+    }
+}
