7b9638eadb1fc2cdafe03901331bdf71dc73474f
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-06-30 08:47:49 +0700

    make middleware api key

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/city/{city:slug}', [CityController::class, 'show']);
-Route::apiResource('/cities', CityController::class);
-Route::get('/office/{officeSpace:slug}', [OfficeSpaceController::class, 'show']);
-Route::apiResource('/offices', OfficeSpaceController::class);
-Route::post('/booking-transactions', [BookingTransactionController::class, 'store']);
-Route::get('/check-booking', [BookingTransactionController::class, 'booking_details']);
+Route::middleware('apikey')->group(function () {
+    Route::get('/city/{city:slug}', [CityController::class, 'show']);
+    Route::apiResource('/cities', CityController::class);
+    Route::get('/office/{officeSpace:slug}', [OfficeSpaceController::class, 'show']);
+    Route::apiResource('/offices', OfficeSpaceController::class);
+    Route::post('/booking-transactions', [BookingTransactionController::class, 'store']);
+    Route::get('/check-booking', [BookingTransactionController::class, 'booking_details']);
+});
