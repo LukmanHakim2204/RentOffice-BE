4f3b36d5a6aa333dee456718d91361f1c6b9e4b0
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-08-13 09:56:15 +0700

    update rent office view

--- app/Filament/Resources/OfficeSpaceResource.php
+++ app/Filament/Resources/OfficeSpaceResource.php
@@ -3,17 +3,15 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\OfficeSpaceResource\Pages;
-use App\Filament\Resources\OfficeSpaceResource\RelationManagers;
 use App\Models\OfficeSpace;
-use Doctrine\DBAL\Query\From;
 use Filament\Forms;
 use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Tabs\Tab;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class OfficeSpaceResource extends Resource
 {
@@ -25,85 +23,114 @@ class OfficeSpaceResource extends Resource
     {
         return $form
             ->schema([
-                Section::make('Office Space')
-                    ->schema([
-                        Forms\Components\TextInput::make('name')
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\FileUpload::make('thumbnail')
-                            ->required()
-                            ->image()
-                            ->maxSize(2048),
-                        Forms\Components\Select::make('city_id')
-                            ->relationship('city', 'name')
-                            ->searchable()
-                            ->preload()
-                            ->required(),
-
-                        Forms\Components\TextInput::make('price')
-                            ->required()
-                            ->numeric()
-                            ->prefix('IDR'),
-                        Forms\Components\Select::make('is_open')
-                            ->options([
-                                true => 'Open',
-                                false => 'Not Open',
-                            ]),
-                        Forms\Components\Select::make('is_fullbook')
-                            ->options([
-                                true => 'Not Available',
-                                false => 'Available',
-                            ])
-                            ->required(),
-                        Forms\Components\TextInput::make('duration')
-                            ->required()
-                            ->numeric()
-                            ->prefix('Days'),
-                        Forms\Components\TextInput::make('address')
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\Textarea::make('about')
-                            ->required()
-                            ->columnSpanFull(),
-                    ])->columns(2),
-
-                Section::make('Photos & Benefits')
-                    ->schema([
-                        Forms\Components\Repeater::make('photos')
-                            ->relationship('photos')
+                Tabs::make('Office Space Tabs')
+                    ->columnSpanFull()
+                    ->tabs([
+                        Tab::make('General')
                             ->schema([
-                                Forms\Components\FileUpload::make('photo')
-                                    ->image()
-                                    ->maxSize(2048)
-                                    ->required(),
-                            ]),
-                        Forms\Components\Repeater::make('benefits')
-                            ->relationship('benefits')
-                            ->schema([
-                                Forms\Components\TextInput::make('name')
-                                    ->required()
-                                    ->maxLength(255),
-                            ]),
-                        Forms\Components\Repeater::make('contacts')
-                            ->relationship('contacts')
-                            ->schema([
-                                Forms\Components\TextInput::make('name')
-                                    ->required()
-                                    ->maxLength(255),
-                                Forms\Components\TextInput::make('position')
-                                    ->required()
-                                    ->maxLength(255),
-                                Forms\Components\TextInput::make('phone')
-                                    ->required()
-                                    ->maxLength(255),
-                                Forms\Components\FileUpload::make('picture')
-                                    ->image()
-                                    ->maxSize(2048)
-                                    ->nullable()
-                                    ->columnSpanFull(),
-                            ]),
-                    ])->columns(2),
+                                Section::make('Office Space')
+                                    ->schema([
+                                        Forms\Components\TextInput::make('name')
+                                            ->required()
+                                            ->maxLength(255),
 
+                                        Forms\Components\FileUpload::make('thumbnail')
+                                            ->required()
+                                            ->image()
+                                            ->maxSize(2048),
+
+                                        Forms\Components\Select::make('city_id')
+                                            ->relationship('city', 'name')
+                                            ->searchable()
+                                            ->preload()
+                                            ->required(),
+
+                                        Forms\Components\TextInput::make('price')
+                                            ->required()
+                                            ->numeric()
+                                            ->prefix('IDR'),
+
+                                        Forms\Components\Select::make('is_open')
+                                            ->options([
+                                                true => 'Open',
+                                                false => 'Not Open',
+                                            ]),
+
+                                        Forms\Components\Select::make('is_fullbook')
+                                            ->options([
+                                                true => 'Not Available',
+                                                false => 'Available',
+                                            ])
+                                            ->required(),
+
+                                        Forms\Components\TextInput::make('duration')
+                                            ->required()
+                                            ->numeric()
+                                            ->prefix('Days'),
+
+                                        Forms\Components\TextInput::make('address')
+                                            ->required()
+                                            ->maxLength(255),
+
+                                        Forms\Components\Textarea::make('about')
+                                            ->required()
+                                            ->columnSpanFull(),
+                                    ])
+                                    ->columns(2),
+                            ]),
+
+                        Tab::make('Photos & Benefits')
+                            ->schema([
+                                Section::make('Photos')
+                                    ->schema([
+                                        Forms\Components\Repeater::make('photos')
+                                            ->relationship('photos')
+                                            ->schema([
+                                                Forms\Components\FileUpload::make('photo')
+                                                    ->image()
+                                                    ->maxSize(2048)
+                                                    ->required(),
+                                            ]),
+                                    ]),
+
+                                Section::make('Benefits')
+                                    ->schema([
+                                        Forms\Components\Repeater::make('benefits')
+                                            ->relationship('benefits')
+                                            ->schema([
+                                                Forms\Components\TextInput::make('name')
+                                                    ->required()
+                                                    ->maxLength(255),
+                                            ]),
+                                    ]),
+                            ]),
+
+                        Tab::make('Contacts')
+                            ->schema([
+                                Section::make('Contact Persons')
+                                    ->schema([
+                                        Forms\Components\Repeater::make('contacts')
+                                            ->relationship('contacts')
+                                            ->schema([
+                                                Forms\Components\TextInput::make('name')
+                                                    ->required()
+                                                    ->maxLength(255),
+                                                Forms\Components\TextInput::make('position')
+                                                    ->required()
+                                                    ->maxLength(255),
+                                                Forms\Components\TextInput::make('phone')
+                                                    ->required()
+                                                    ->maxLength(255),
+                                                Forms\Components\FileUpload::make('picture')
+                                                    ->image()
+                                                    ->maxSize(2048)
+                                                    ->nullable()
+                                                    ->columnSpanFull(),
+                                            ]),
+                                    ]),
+                            ]),
+
+                    ]),
             ]);
     }
 
